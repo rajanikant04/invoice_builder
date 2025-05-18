2c2fe50f197b4153e93e9a65ee653a55b3b0fa2e
Rajanikant04 <rajanikantsinghnitr@gmail.com>
2025-05-19 00:00:26 +0530

    modify

--- app/page.tsx
+++ app/page.tsx
@@ -68,7 +68,7 @@ const InvoiceGenerator = () => {
   const total = subtotal + taxAmount - discountAmount;
   const balance = total;
 
-  function formatDate(date) {
+  function formatDate(date:any) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = String(date.getMonth() + 1).padStart(2, '0');
     const year = date.getFullYear();
